93bf2e04a1506c83ef1ac477e10bc071a6b018b8
MacBookPro <matthewpalmer9@outlook.com>
2020-11-30 11:03:21 -0600

    added a styled brand/navbar with a (currently) non-functional hamburger menu.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
+import Index from './components/Index.jsx';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app-container">
-        <Navbar/>
-      </div>
+      <>
+        <Router>
+          <Navbar />
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+        </Router>
+      </>
     )
   }
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,49 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.div`
+    display: flex;
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.9);
+    height: 60px;
+`;
+
+const Brand = styled.span`
+    display: flex;
+    height: 100%;
+    width: 100%;
+    font-size: 24px;
+    font-weight: bold;
+    margin: 0 50px 0 0;
+    justify-content: center;
+    align-items: center;
+    color: aqua;
+
+    text-shadow: 0 0 5px aqua;
+`;
 
 export default class Navbar extends PureComponent {
     render() {
         return (
-            <div className="navbar">
-                Hello from Navbar
-            </div>
+            <>
+                <Nav>
+                    <div className="navbar">
+                        <Brand>
+                            <div className="brand">
+                                    <span>MattPDev</span>
+                            </div>
+                        </Brand>
+
+                        <div className="menu">
+                                <div className="one"></div>
+                                <div className="two"></div>
+                                <div className="three"></div>
+                        </div>
+                    </div>
+
+
+                </Nav>
+            </>
         )
     }
 }
