9b03e1448358c8ae15214cf8c5d81a6d50f1c8a0
MacBookPro <matthewpalmer9@outlook.com>
2020-11-30 09:42:36 -0600

    Created a components folder to separate concerns and created a Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Navbar from './components/Navbar.jsx';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello from App
+      <div className="app-container">
+        <Navbar/>
       </div>
     )
   }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Navbar extends PureComponent {
+    render() {
+        return (
+            <div className="navbar">
+                Hello from Navbar
+            </div>
+        )
+    }
+}
