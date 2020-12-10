751594e5fccb61d3c919f6dd2146810b520ab64d
MacBookPro <matthewpalmer9@outlook.com>
2020-12-10 11:36:16 -0600

    Resubmitting commit due to deployment error.

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,7 +17,7 @@ export default class About extends PureComponent {
 
                     <div className="about-content-right">
                         <h1>Front-end Website Developer</h1>
-                        <p>Hi there!</p>
+                        <p>Hi there!</p> 
                         <p>My name is Matthew Ryan Palmer, and I'm a Front-end Website Developer living in the Austin, TX area.</p>
                         <p>I started my journey from "The Web Developer Bootcamp" by Colt Steele on Udemy. Ever since, I've fallen in love with web development and went to school twice for it -- Once at Purdue University Global and again at Flatiron School. Since graduating, I've spent most of my free time taking my craft to the next level. Like many other people, I enjoy spending my free time increasing my knowledge of web development and building new and challenging projects.</p>
                     </div>
