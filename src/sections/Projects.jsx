132016caac00e43a6fadce5f4aa8955b817a2761
MacBookPro <matthewpalmer9@outlook.com>
2020-12-18 23:28:40 -0600

    Separated projects into functional components for clearner code.

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import Project from './Project.jsx';
 import dragndropPic from '../project-imgs/react-dragndrop.png';
 import weatherappPic from '../project-imgs/mattpdev-wa.png';
 import rgbcolorgame from '../project-imgs/rgbcolorgame.png';
@@ -12,92 +13,53 @@ export default class Projects extends PureComponent {
                 <div className="hr"></div>
 
                 <div className="projects-container">
-                    <div className="project">
-                        <a target="_blank" rel="noopener noreferrer" href="https://react-drag-drop-five.vercel.app/">
-                            <img alt="react-dragndrop" src={dragndropPic} />
-                        </a>
-                        <div className="project-description">
-                        <a target="_blank" rel="noopener noreferrer" href="https://react-drag-drop-five.vercel.app/">
-                            <h2>React Drag N' Drop</h2>
-                        </a>
-                            <span className="tech-list">
-                                <strong>Technologies: </strong>
-                                <span className="javascript">JavaScript</span>,
-                                <span className="css">CSS</span>,
-                                <span className="html">HTML</span>,
-                                <span className="react">React</span>, 
-                            </span>
-                        </div>
-                        <div className="hr"></div> 
-                        <p className="description">
-                            A UI/UX designed using the HTML5 "Drag n Drop API". <strong>Currently not mobile friendly due to this APIs capatability.</strong>
-                        </p>
-                    </div>
+                    <Project
+                        demoLink="https://react-drag-drop-five.vercel.app/"
+                        imgSrc={dragndropPic}
+                        title="React Drag N' Drop"
+                        techs={["JavaScript", "CSS", "HTML", "React"]}
+                        description={
+                            <p className="description">
+                                A UI/UX designed using the HTML5 "Drag n Drop API". <strong>Currently not mobile friendly due to this APIs capatability.</strong>
+                            </p>
+                        }
+                    />
 
-                    <div className="project">
-                        <a target="_blank" rel="noopener noreferrer" href="https://mattpdev-weather-app.vercel.app/">
-                            <img alt="weatherapp" src={weatherappPic} />
-                        </a>
-                        <div className="project-description">
-                        <a target="_blank" rel="noopener noreferrer" href="https://mattpdev-weather-app.vercel.app/">
-                            <h2>Weather App</h2>
-                        </a>
-                            <span className="tech-list">
-                                <strong>Technologies: </strong>
-                                <span className="javascript">JavaScript</span>, 
-                                <span className="css">CSS</span>,
-                                <span className="html">HTML</span>,
-                                <span className="react">React</span>, 
-                            </span>
-                        </div>
-                        <div className="hr"></div> 
-                        <p className="description">
-                            A weather app utilizing the OpenWeatherMap API. Gathers live weather information from anywhere around the world.
-                        </p>
-                    </div>
+                    <Project
+                        demoLink="https://mattpdev-weather-app.vercel.app/"
+                        imgSrc={weatherappPic}
+                        title="Weather App"
+                        techs={["JavaScript", "CSS", "HTML", "React"]}
+                        description={
+                            <p className="description">
+                                A weather app utilizing the OpenWeatherMap API. Gathers live weather information from anywhere around the world.
+                            </p>
+                        }
+                    />
 
-                    <div className="project">
-                        <a target="_blank" rel="noopener noreferrer" href="https://rgbcolorgame.vercel.app/">
-                            <img alt="rgbcolorgame" src={rgbcolorgame} />
-                        </a>
-                        <div className="project-description">
-                        <a target="_blank" rel="noopener noreferrer" href="https://rgbcolorgame.vercel.app/">
-                            <h2>RGB Color Guessing Game</h2>
-                        </a>
-                            <span className="tech-list">
-                                <strong>Technologies: </strong>
-                                <span className="javascript">JavaScript</span>, 
-                                <span className="css">CSS</span>,
-                                <span className="html">HTML</span>,
-                                <span className="react">React</span>, 
-                            </span>
-                        </div>
-                        <div className="hr"></div> 
-                        <p className="description">
-                            "The Great RGB Color Guessing Game" originaly created by Colt Steele and remade using React.
-                        </p>
-                    </div>
+                    <Project
+                        demoLink="https://rgbcolorgame.vercel.app/"
+                        imgSrc={rgbcolorgame}
+                        title="The Great RGB Color Guessing Game"
+                        techs={["JavaScript", "CSS", "HTML", "React"]}
+                        description={
+                            <p className="description">
+                                "The Great RGB Color Guessing Game" originaly created by Colt Steele and remade using React.
+                            </p>
+                        }
+                    />
 
-                    <div className="project">
-                        <a target="_blank" rel="noopener noreferrer" href="https://mrp-carousel.vercel.app/">
-                            <img alt="rgbcolorgame" src={JSSlider} />
-                        </a>
-                        <div className="project-description">
-                        <a target="_blank" rel="noopener noreferrer" href="https://mrp-carousel.vercel.app/">
-                            <h2>JS Slider</h2>
-                        </a>
-                            <span className="tech-list">
-                                <strong>Technologies: </strong>
-                                <span className="javascript">JavaScript</span>, 
-                                <span className="css">CSS</span>,
-                                <span className="html">HTML</span>,
-                            </span>
-                        </div>
-                        <div className="hr"></div> 
-                        <p className="description">
-                            An image slider built 100% in vanilla JavaScript, CSS and HTML. Eliminates the heavy footprint of using a framework or library.
-                        </p>
-                    </div>
+                    <Project
+                        demoLink="https://mrp-carousel.vercel.app/"
+                        imgSrc={JSSlider}
+                        title="JS Slider"
+                        techs={["JavaScript", "CSS", "HTML"]}
+                        description={
+                            <p className="description">
+                                An image slider built 100% in vanilla JavaScript, CSS and HTML. Eliminates the heavy footprint of using a framework or library.
+                            </p>
+                        }
+                    />
                 </div>
             </div>
         )
