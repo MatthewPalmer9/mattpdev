78c880caeba5afe5ff7b06872abb780905aaa8a8
Matthew Palmer <matthewpalmer9@outlook.com>
2022-01-22 14:47:55 -0600

    Updating design and re-doing portfolio.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header.jsx';
-import SectionHero from './components/hero/SectionHero.jsx';
 import Main from './components/Main.jsx';
 
 function App() {
