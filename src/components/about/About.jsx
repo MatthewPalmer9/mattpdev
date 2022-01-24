8b4d5e50239c9a1997a78f052dae6d67e89c933f
Matthew Palmer <matthewpalmer9@outlook.com>
2022-01-24 10:39:16 -0600

    added an 02 section to the About section.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,5 @@
 import familyPhoto from './the-family1.png';
+import NoraleeAndI from './NoraleeAndI.png';
 
 export default function About() {
     return (
@@ -6,7 +7,7 @@ export default function About() {
             <div className="container">
                 <p className="about-label">ABOUT</p>
                 <h2 className="heading-secondary">Who am I?</h2>
-                <div className="grid-two-cols mg-top center-v">
+                <div className="grid-two-cols row-gap mg-top center-v">
                     {/* STEP 01 TEXT */}
                     <div className="about-text-container">
                         <p className="number">01</p>
@@ -20,6 +21,22 @@ export default function About() {
                     {/* STEP 01 IMAGE */}
                     <div className="about-img-container">
                         <img className="about-img" src={familyPhoto} alt="Matt and his family" />
+                    </div>
+
+                    {/* STEP 02 IMAGE */}
+                    <div className="about-img-container">
+                        <img className="about-img" src={NoraleeAndI} alt="Matt and his family" />
+                    </div>
+
+                    {/* STEP 02 TEXT */}
+                    <div className="about-text-container">
+                        <p className="number">02</p>
+                        <h3>A Front-end Developer</h3>
+                        <p className="about-description">
+                            I love to design and develop websites! I am focused on UI/UX, visual hierarchy, 
+                            typography, and ultimately mobile responsiveness. Websites all have a personality 
+                            that tell a story, and I take pride in helping you tell yours. 
+                        </p>
                     </div>
                 </div>
             </div>
